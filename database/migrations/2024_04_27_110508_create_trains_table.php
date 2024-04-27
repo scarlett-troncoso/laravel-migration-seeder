d3f914f147c31fea8dc84d1774eb36274cff2eb3
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-27 16:30:35 +0200

    add columns of table trains

--- database/migrations/2024_04_27_110508_create_trains_table.php
+++ database/migrations/2024_04_27_110508_create_trains_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->tinyText('azienda', 150); // tinyTExt fino a 255 caratteri
+            $table->string('stazione_partenza', 255);
+            $table->string('stazione_arrivo', 255);
+            $table->dateTime('orario_partenza');
+            $table->dateTime('orario_arrivo');
+            $table->string('durata_totale', 15);
+            $table->unsignedTinyInteger('codice_treno');
+            $table->unsignedTinyInteger('carrozza')->nullable(); //nullable perche la carrozza puo cambiare a ultimo momento
+            $table->unsignedTinyInteger('quantita_fermate');
+            $table->tinyInteger('quantita_cambi')->unsigned();
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
