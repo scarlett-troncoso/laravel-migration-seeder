7061dd0be8b70dc677dfc7ec7ffccd9477b25cd0
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-29 17:44:48 +0200

    create new folder partials and files header and footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,29 @@
+<footer class="bg-light py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h4>Company</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima veritatis quae at quidem atque
+                    nulla autem explicabo dolorum. Ipsum, fugit? Saepe odit sapiente unde facilis iure! Enim ipsa
+                    vero pariatur.</p>
+            </div>
+            <div class="col">
+                <h4>Quick Links</h4>
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem</a></li>
+                    <li><a href="">Lorem</a></li>
+                    <li><a href="">Lorem</a></li>
+                    <li><a href="">Lorem</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>Legal</h4>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+            </div>
+        </div>
+    </div>
+
+</footer>
