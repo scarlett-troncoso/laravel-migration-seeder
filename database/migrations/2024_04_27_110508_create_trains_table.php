93e603a9b22965707bda6bdde4c747f8037729e7
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-29 16:20:27 +0200

    changes of columns types and command migrate

--- database/migrations/2024_04_27_110508_create_trains_table.php
+++ database/migrations/2024_04_27_110508_create_trains_table.php
@@ -18,14 +18,36 @@ return new class extends Migration
             $table->string('stazione_arrivo', 255);
             $table->dateTime('orario_partenza');
             $table->dateTime('orario_arrivo');
-            $table->string('durata_totale', 15);
-            $table->unsignedTinyInteger('codice_treno');
-            $table->unsignedTinyInteger('carrozza')->nullable(); //nullable perche la carrozza puo cambiare a ultimo momento
-            $table->unsignedTinyInteger('quantita_fermate');
-            $table->tinyInteger('quantita_cambi')->unsigned();
-            $table->boolean('in_orario');
-            $table->boolean('cancellato');
+            $table->string('durata_totale', 9);
+            $table->unsignedSmallInteger('codice_treno');
+            $table->unsignedTinyInteger('carrozza')->nullable(); //nullable perche la carrozza puo cambiare
+            $table->unsignedTinyInteger('quantita_fermate')->nullable();
+            $table->tinyInteger('quantita_cambi')->default(0)->unsigned();
+            $table->boolean('in_orario')->default(true); //utilizzare le parole chiave true e false nelle query del database
+            $table->boolean('cancellato')->default(false); //utilizzare le parole chiave true e false nelle query del database
             $table->timestamps();
+
+            /*
+            $table->string('classi', 15);
+            $table->decimal('price', 6)->unsigned();
+            */
+
+            /* INSERIRE DATI NELL DB ↓↓↓
+            $train = new App\Models\Train();
+                $train->stazione_partenza = "Milano Centrale"
+                $train->stazione_arrivo = "Roma Termini"
+                $train->azienda = "FrecciaRossa"
+                $train->orario_partenza = "2024-04-25 06:15:00"
+                $train->orario_arrivo = "2024-04-25 11:15:00"
+                $train->durata_totale = "05h 00min"
+                $train->codice_treno = 9584
+                $train->carrozza = 25
+                $train->quantita_fermate = 6
+                $train->quantita_cambi = 1
+            $train->save();
+
+            $train->in_orario = false
+            */
         });
     }
 
