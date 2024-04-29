1103eeea7fee54696216164d85dec4106a959348
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-29 17:47:12 +0200

    include partials header and footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,20 +12,21 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- Styles -->
-    @yield(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
 <body class="antialiased">
-    <header>
-        Header
-    </header>
+    @include('partials.header')
     <main>
         @yield('content')
     </main>
-    <footer>
-        Footer
-    </footer>
+    @include('partials.footer')
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
